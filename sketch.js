939c855e122325d4b850daa9b30213a2542f1b11
jogurtas <kisielius.mykolas@gmail.com>
2019-12-15 18:38:36 +0200

    imod lib compiled to WASM

--- sketch.js
+++ sketch.js
@@ -9,21 +9,35 @@ function preload() {
 
 async function setup() {
   const canvas = createCanvas(img.width, img.height);
+  pixelDensity(1);
   canvas.parent("canvas");
   textFont("Roboto");
 
   image(img, 0, 0);
   loadPixels();
-  console.log(pixels);
+  //console.log(pixels);
 
   // WASM
   var imod = await loadLib();
-  const p = api.create_buffer(canvas.width, canvas.height);
+  const p = imod.create_buffer(canvas.width, canvas.height);
   Module.HEAP8.set(pixels, p);
-  // ... call encoder ...
-  api.destroy_buffer(p);
+  var w = imod.green(p, canvas.width, canvas.height, 4);
 
-  console.log(imod.version());
+  const resultPointer = imod.get_result_pointer();
+  const resultSize = imod.get_result_size();
+  const resultView = new Uint8Array(Module.HEAP8.buffer, resultPointer, resultSize);
+  const result = new Uint8Array(resultView);
+  
+  console.log(pixels);
+
+  copyPixelArray(result); 
+  
+  //console.log("Width: " + w);
+  //console.log("Ptr: " + p);
+  //imod.destroy_buffer(p);
+
+  console.log(pixels);
+  //console.log(imod.version());
 
   updatePixels();
 }
@@ -32,16 +46,16 @@ function draw() {
   //image(img, 0, 0);
 }
 
-function loadImageData(fx, sliderValue) {
+function copyPixelArray(data) {
   loadPixels();
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
       let index = (x + y * width) * 4;
 
-      pixels[index] = colors[0];
-      pixels[index + 1] = colors[1];
-      pixels[index + 2] = colors[2];
+      pixels[index] = data[index];
+      pixels[index + 1] = data[index + 1];
+      pixels[index + 2] = data[index + 2];
       pixels[index + 3] = 255;
     }
   }
@@ -55,8 +69,9 @@ function loadLib() {
       version: Module.cwrap("version", "number", []),
       create_buffer: Module.cwrap("create_buffer", "number", ["number", "number"]),
       destroy_buffer: Module.cwrap("destroy_buffer", "", ["number"]),
-      green: Module.cwrap("green", "number", []),
-      fileString: Module.cwrap("fileString", "string", [])
+      green: Module.cwrap("green", "", ["number", "number", "number", "number"]),
+      get_result_pointer: Module.cwrap("get_result_pointer", "number", []),
+      get_result_size: Module.cwrap("get_result_size", "number", [])
     };
     resolve(imod);
   });
